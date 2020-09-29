95f6bbd18b8a255b07839e0611ca6be14dd4c67f
lenya <66315146+Lev0nid@users.noreply.github.com>
2020-09-29 19:21:59 +0300

    Первая домашка

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+double areaBySimpson(double leftBorder, double rightBorder, int amountOfRectangles) {
+    double subgraphArea = 0, rectangleSize = (rightBorder - leftBorder) / amountOfRectangles;
+    for(int i = 1; i < amountOfRectangles; i+=2) {
+        subgraphArea += sin(leftBorder + (i - 1) * rectangleSize) + 4 * sin(leftBorder + i * rectangleSize) + sin(leftBorder + (i + 1) * rectangleSize);
+    }
+    return subgraphArea * rectangleSize / 3;
+}
+
+double areaByRectangleMethod(double leftBorder, double rightBorder, int amountOfRectangles) {
+    double subgraphArea = 0, rectangleSize = (rightBorder - leftBorder) / amountOfRectangles;
+    for(int j = 0; j < amountOfRectangles; j++) {
+        subgraphArea += sin(leftBorder + (j + 0.5) * rectangleSize) * rectangleSize;
+    }
+    return subgraphArea;
+}
+
+int main() {
+    //Считываем границы
+    double leftBorder, rightBorder;
+    scanf("%lf%lf", &leftBorder, &rightBorder);
+
+    //Подсчет площади и вывод
+    int amountOfCases = 6;
+    int experiments[6] = {5, 10, 20, 100, 500, 1000};
+    double results[6][2];
+    for(int i = 0; i < amountOfCases; i++) {
+        results[i][0] = areaByRectangleMethod(leftBorder, rightBorder, experiments[i]);
+        results[i][1] = areaBySimpson(leftBorder, rightBorder, experiments[i]);
+        printf("%i%c", experiments[i], ' ');
+        printf("%.5f%c", results[i][0], ' ');
+        printf("%.5f\n", results[i][1]);
+    }
+    return 0;
+}
